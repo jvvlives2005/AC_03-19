3df9062dc20af5fd3fba09cebed4654189c7a419
jvvlives2005 <jvvlives2005@aol.com>
2015-03-19 19:21:06 -0400

    initial commit 3-19

---
+++ src/nyc/c4q/jvvlives2005/Person.java
@@ -0,0 +1,41 @@
+package nyc.c4q.jvvlives2005;
+
+/**
+ * Access Code 2.1
+ *
+ * Author  Gregory Gundersen
+ * Date    March 2015
+ *
+ * This class represents a person with a name and age.
+ */
+
+public class Person {
+
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAge(int newAge) {
+        this.age = newAge;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
